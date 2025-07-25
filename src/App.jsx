17180db22087b6b17c5d442d8f5f0d4eed2b4fc1
Nilam Yadav <nilam.yadav@borngroup.com>
2025-07-25 18:02:53 +0530

    initial commit for ecommerce react + tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     {/* {fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => console.log(data))
+      } */}
+    </>
+  )
+}
+
+export default App
